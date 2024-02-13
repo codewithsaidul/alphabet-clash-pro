8c7bcf24c553b90e9a630efbca9dd53d76d6626b
codewithsaidul <codewithsaidul@gmail.com>
2024-02-13 21:52:16 +0600

    Generate a Random Alphabet Sucessfuly

--- js/main.js
+++ js/main.js
@@ -9,13 +9,40 @@ function showElement (element) {
     showScreen.classList.remove('hidden')
 }
 
+function getRandomAlphabet () {
+    // Create an Alphabet Array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabets = alphabetString.split('');
+    
 
-function play () {
+    // Get A Random Index 0 to 51
+    const randomNumber = Math.random() * 51;
+    const randomIndex = Math.round(randomNumber);
+
+    // Get a Random Alphabet
+    const alphabet = alphabets[randomIndex];
+    return alphabet;
+}
+
+
+function startGame () {
+    // Step 1 : Generate a Random Alphabet
+    getRandomAlphabet();
+}
+
+
+function enterTheGame () {
     // Hide The Home Screen.
     hideElement('home')
 
 
     //Show the Play Ground Area
-    showElement('play-ground')
+    showElement('play-ground');
+
+
+    // Start The Game
+    startGame();
 }
+
+
 
